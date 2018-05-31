2c3290bbe44e2f404273aae723616bef43d186a7
George  Pianka <gpianka1@gmail.com>
2018-05-31 04:09:35 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -2,8 +2,11 @@ class EmailParser
   attr_accessor :email_string 
   
   def initialize(email_string)
+    @email_string = email_string
+  end
   
   def parse 
+    self.email_string.splice(/[])
     
     
     
